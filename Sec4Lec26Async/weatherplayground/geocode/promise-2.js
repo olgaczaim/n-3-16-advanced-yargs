37832deeba4486425835eead9d8bb48f382ea51a
Olgac Zaim <46277089+olgaczaim@users.noreply.github.com>
2019-01-28 15:51:19 +0300

    update

---
+++ Sec4Lec26Async/weatherplayground/geocode/promise-2.js
@@ -0,0 +1,27 @@
+const request= require('request');
+
+var geocodeAddress = (address) => {
+    return new Promise((res, rej) => {
+        var address = encodeURIComponent(address);
+        request({
+            url: `http://www.mapquestapi.com/geocoding/v1/address?key=pAENXEQxk0fFTaRsILYPWQrpe0FsImmX&location=${address}`,
+            json: true,
+            proxy: 'http://proxy.tnb.org:8080'
+        }, (error, response, body) => {
+            if (error){
+                rej('Unable to connect to service');
+            } else {
+                res({
+                    lat: body.results[0].locations[0].latLng.lat,
+                    lng: body.results[0].locations[0].latLng.lng,
+                });
+            }
+        });
+    });
+};
+
+geocodeAddress('ankara').then((location) => {
+    console.log(JSON.stringify(location, undefined, 2));
+}, (err) => {
+   console.log(err);
+});
